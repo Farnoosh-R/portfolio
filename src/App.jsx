5f48223a099f5b5b6fab3684c8ca2a0aa1aa5574
Farnoush Ramezanali <f.ramezanali@amnmoj.com>
2026-05-20 12:06:39 +0330

    second

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import { FaInstagram } from "react-icons/fa";
 import { FaTelegramPlane } from "react-icons/fa";
 import { FaWhatsapp } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
-import slide1 from "./assets/images/layout/slide1.webp";
-import slide2 from "./assets/images/layout/slide2.webp";
-import slide3 from "./assets/images/layout/slide3.jpg";
+import slide1 from "./assets/images/layout/slide1.png";
+import slide2 from "./assets/images/layout/slide2.png";
+import slide3 from "./assets/images/layout/slide3.png";
 import SwiperSlider from "./components/ui/slider/SwiperSlider";
+import { HiCheck } from "react-icons/hi";
+import { FaLightbulb } from "react-icons/fa";
 
 export function useScrollAnimation() {
   useEffect(() => {
@@ -58,7 +60,7 @@ function App() {
     },
     {
       id: 4,
-      image: slide3,
+      image: slide1,
       title: "Luxury Resort",
       buttonText: "Explore",
     },
@@ -68,7 +70,7 @@ function App() {
     <>
       <section
         id="main"
-        className="relative h-screen overflow-x-hidden bg-cover overflow-y-auto flex items-center justify-center scroll-anim"
+        className="relative min-h-[100dvh] bg-cover overflow-y-auto flex flex-col gap-5 items-center justify-center scroll-anim"
         style={{
           backgroundImage: `url(${bgImage})`,
           "--from": "scale(0.9)",
@@ -77,15 +79,15 @@ function App() {
         <div className="absolute inset-0 bg-black/50 z-0" />
         <div className="relative app-container grid grid-cols-1 lg:grid-cols-2 border-3 border-white rounded-4xl overflow-hidden">
           <div className="flex flex-col gap-4 p-10">
-            <h1 className="text-white leading-none text-left">TAL</h1>
+            <h1 className="text-white text-left drop-shadow-2xl -ml-9">TAL</h1>
             <SwiperSlider items={slides} />
           </div>
 
-          <div className="relative pl-10 pr-2 pt-10 pb-10 flex flex-col gap-7 text-white">
+          <div className="relative p-10 flex flex-col gap-7 text-white">
             <div className="absolute inset-0 bg-white/10 backdrop-blur-md z-0" />
 
             <div className="relative z-10 flex flex-col gap-7">
-              <div>
+              <div className="-mr-10">
                 <h1
                   className="bg-center bg-cover bg-clip-text text-transparent drop-shadow-2xl"
                   style={{ backgroundImage: `url(${bgImage})` }}
@@ -100,48 +102,108 @@ function App() {
                   DREAMS
                 </h1>
               </div>
-              <div className="flex flex-col gap-5 pr-10">
+              <div className="flex flex-col gap-10">
                 <h4 className="text-[var(--text-secondary)]">
                   ترکیب طراحی و کدنویسی برای ساخت تجربه‌های دیجیتال جذاب و
                   کاربردی
                 </h4>
-                <div className="text-white">
-                  تمرکز من روی ساخت UI های مدرن، سبک و کاربرمحور است
+
+                <div className="flex flex-col gap-5">
+                  <div className="flex gap-1">
+                    <FaLightbulb color="var(--brand-primary)" size={15} />
+                    <div className="text-white">
+                     ساخت UI های مدرن، سبک و کاربرمحور
+                    </div>
+                  </div>
+                  <div className="flex gap-1">
+                    <FaLightbulb color="var(--brand-primary)" size={15} />
+                    <div className="text-white">
+                    هر پروژه نتیجه ترکیب طراحی و کدنویسی تمیز
+                    </div>
+                  </div>
+                  <div className="flex gap-1">
+                    <FaLightbulb color="var(--brand-primary)" size={15} />
+                    <div className="text-white">
+                    تبدیل ایده‌ها به رابط‌های واقعی و قابل استفاده
+                    </div>
+                  </div>
                 </div>
               </div>
-              <div className="flex justify-end gap-3">
-                <button className="text-lg bg-[var(--brand-primary)] hover:bg-[#799c02] px-5 py-4 rounded-2xl cursor-pointer">
-                  تماس بگیرید
-                </button>
-              </div>
-              <div className="flex justify-end gap-4">
-                <a
-                  className="scroll-anim cursor-pointer p-2 hover:border hover:rounded-full transition-all duration-300 hover:text-white-500 hover:scale-125 hover:drop-shadow-[0_0_10px_rgba(197,257,0,1)]"
-                  style={{ "--from": "translateX(-60px)" }}
-                >
-                  <FaInstagram color="var(--text-primary)" size={20} />
-                </a>
-                <a
-                  className="scroll-anim cursor-pointer p-2 hover:border hover:rounded-full transition-all duration-300 hover:text-white-500 hover:scale-125 hover:drop-shadow-[0_0_10px_rgba(197,257,0,1)]"
-                  style={{ "--from": "translateX(-60px)" }}
-                >
-                  <FaTelegramPlane color="var(--text-primary)" size={20} />
-                </a>
-                <a
-                  className="scroll-anim cursor-pointer p-2 hover:border hover:rounded-full transition-all duration-300 hover:text-white-500 hover:scale-125 hover:drop-shadow-[0_0_10px_rgba(197,257,0,1)]"
-                  style={{ "--from": "translateX(-60px)" }}
-                >
-                  <FaWhatsapp color="var(--text-primary)" size={20} />
-                </a>
-                <a
-                  className="scroll-anim cursor-pointer p-2 hover:border hover:rounded-full transition-all duration-300 hover:text-white-500 hover:scale-125 hover:drop-shadow-[0_0_10px_rgba(197,257,0,1)]"
-                  style={{ "--from": "translateX(-60px)" }}
-                >
-                  <FaLinkedin color="var(--text-primary)" size={20} />
-                </a>
+              <div className="flex gap-10 justify-between mt-15">
+                <div>
+                  <div className="flex flex-col items-end gap-3 items-center">
+                    <div className="flex gap-1">
+                      <a
+                        href="https://farnoosh-r.github.io/react-demo1"
+                        target="_blank"
+                        className="hover:text-[var(--brand-primary)]"
+                      >
+                        demo1.farnooshstudio.ir
+                      </a>
+                      <HiCheck color="var(--brand-primary)" size={15} />
+                    </div>
+                    <div className="flex gap-1">
+                      <a
+                        href="#"
+                        target="_blank"
+                        className="hover:text-[var(--brand-primary)]"
+                      >
+                        demo2.farnooshstudio.ir
+                      </a>
+                      <HiCheck color="var(--brand-primary)" size={15} />
+                    </div>
+                    <div className="flex gap-1">
+                      <a
+                        href="#"
+                        target="_blank"
+                        className="hover:text-[var(--brand-primary)]"
+                      >
+                        demo3.farnooshstudio.ir
+                      </a>
+                      <HiCheck color="var(--brand-primary)" size={15} />
+                    </div>
+                  </div>
+                </div>
+                <div className="flex flex-col gap-7 items-end">
+                  <button className="w-fit text-lg bg-[var(--brand-primary)] hover:bg-[#799c02] px-5 py-3 rounded-2xl cursor-pointer">
+                    تماس بگیرید
+                  </button>
+                  <div className="flex justify-end gap-4">
+                    <a
+                      className="scroll-anim cursor-pointer p-2 hover:border hover:rounded-full transition-all duration-300 hover:text-white-500 hover:scale-125 hover:drop-shadow-[0_0_10px_rgba(197,257,0,1)]"
+                      style={{ "--from": "translateX(-60px)" }}
+                    >
+                      <FaInstagram color="var(--text-primary)" size={20} />
+                    </a>
+                    <a
+                      className="scroll-anim cursor-pointer p-2 hover:border hover:rounded-full transition-all duration-300 hover:text-white-500 hover:scale-125 hover:drop-shadow-[0_0_10px_rgba(197,257,0,1)]"
+                      style={{ "--from": "translateX(-60px)" }}
+                    >
+                      <FaTelegramPlane color="var(--text-primary)" size={20} />
+                    </a>
+                    <a
+                      className="scroll-anim cursor-pointer p-2 hover:border hover:rounded-full transition-all duration-300 hover:text-white-500 hover:scale-125 hover:drop-shadow-[0_0_10px_rgba(197,257,0,1)]"
+                      style={{ "--from": "translateX(-60px)" }}
+                    >
+                      <FaWhatsapp color="var(--text-primary)" size={20} />
+                    </a>
+                    <a
+                      className="scroll-anim cursor-pointer p-2 hover:border hover:rounded-full transition-all duration-300 hover:text-white-500 hover:scale-125 hover:drop-shadow-[0_0_10px_rgba(197,257,0,1)]"
+                      style={{ "--from": "translateX(-60px)" }}
+                    >
+                      <FaLinkedin color="var(--text-primary)" size={20} />
+                    </a>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
+        </div>
+        <div className="flex gap-1">
+          <div className="text-[var(--brand-primary)]">Farnoosh</div>
+          <div>Designed & Developed by</div>
+          <div>|</div>
+          <div>2026©</div>
         </div>
       </section>
     </>
--- src/components/ui/slider/SwiperSlider.jsx
+++ src/components/ui/slider/SwiperSlider.jsx
@@ -2,20 +2,31 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import { Pagination } from "swiper/modules";
 import "swiper/css/pagination";
+import { Navigation, Autoplay  } from "swiper/modules";
+import "swiper/css/navigation";
+import { IoChevronBack, IoChevronForward } from "react-icons/io5";
 
 export default function SwiperSlider({ items }) {
   return (
     <div className="w-full mr-auto min-w-0 mr-auto">
       <Swiper
         slidesPerView={1}
-        modules={[Pagination]}
-        pagination={{ clickable: true }}
+        modules={[Navigation, Autoplay]}
+        navigation={{
+          nextEl: ".custom-next",
+          prevEl: ".custom-prev",
+        }}
+            autoplay={{
+        delay: 2500,
+        disableOnInteraction: false,
+      }}
+        slidesPerView={3}
         breakpoints={{
           320: {
             slidesPerView: 1,
           },
           768: {
-            slidesPerView: 1,
+            slidesPerView: 2,
           },
           1024: {
             slidesPerView: 3,
@@ -30,13 +41,23 @@ export default function SwiperSlider({ items }) {
                 <img
                   src={item.image}
                   alt={item.title}
-                  className="w-fit object-cove rounded-3xl"
+                  className=" object-cove rounded-3xl w-[202px] h-[500px]"
                 />
               </div>
             </div>
           </SwiperSlide>
         ))}
       </Swiper>
+      {/* navigation */}
+      <div className="flex items-center justify-start gap-4 mt-10">
+        <button className="custom-prev bg-[var(--brand-primary)] text-white p-2 rounded-lg cursor-pointer">
+          <IoChevronForward size={25}/>
+        </button>
+
+        <button className="custom-next bg-[var(--brand-primary)] text-white p-2 rounded-lg cursor-pointer">
+          <IoChevronBack size={25}/>
+        </button>
+      </div>
     </div>
   );
 }
